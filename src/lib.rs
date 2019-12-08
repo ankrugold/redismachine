582eae07bb3168490fbe0246bacf7fc18273f31b
Ankur Goel <ankrugold@gmail.com>
2019-12-08 23:23:27 +0530

    adding persistence

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,22 @@ extern crate redis_module;
 use redis_module::native_types::RedisType;
 use redis_module::{Context, NextArg, RedisError, RedisResult, REDIS_OK};
 use std::os::raw::c_void;
-
+use crate::machine::SMachine;
+use std::collections::HashMap;
 pub mod machine;
+
+
+
 
 static SM_REDIS_TYPE: RedisType = RedisType::new(
     "smrd_type",
     0,
     raw::RedisModuleTypeMethods {
         version: raw::REDISMODULE_TYPE_METHOD_VERSION as u64,
-        rdb_load: None,
-        rdb_save: None,
+        rdb_load: Some(machine::RedisMachineRdbLoad),
+        rdb_save: Some(machine::RedisMachineRdbSave),
         aof_rewrite: None,
-        free: Some(free),
+        free: Some(machine::Free),
 
         // Currently unused by Redis
         mem_usage: None,
@@ -27,10 +31,6 @@ static SM_REDIS_TYPE: RedisType = RedisType::new(
         aux_save_triggers: 0,
     },
 );
-
-unsafe extern "C" fn free(value: *mut c_void) {
-    Box::from_raw(value as *mut machine::SMachine);
-}
 
 fn sm_new(ctx: &Context, args: Vec<String>) -> RedisResult {
     let mut args = args.into_iter().skip(1);
@@ -50,6 +50,7 @@ fn sm_new(ctx: &Context, args: Vec<String>) -> RedisResult {
         }
     }
 }
+
 fn sm_get(ctx: &Context, args: Vec<String>) -> RedisResult {
     let mut args = args.into_iter().skip(1);
     let name = args.next_string()?;
@@ -64,7 +65,6 @@ fn sm_get(ctx: &Context, args: Vec<String>) -> RedisResult {
         }
         None => {
             Err("machine doesn't exists".into())
-
         }
     }
 }
@@ -76,7 +76,7 @@ fn sm_add(ctx: &Context, args: Vec<String>) -> RedisResult {
     let start = args.next_string()?;
     let end = args.next_string()?;
     let action = args.next_string()?;
-    ctx.log_debug(format!("name: {} event: {} start: {} end: {} action: {} ", name, event, start , end, action).as_str());
+    ctx.log_debug(format!("name: {} event: {} start: {} end: {} action: {} ", name, event, start, end, action).as_str());
     let key = ctx.open_key_writable(&name);
 
     match key.get_value::<machine::SMachine>(&SM_REDIS_TYPE)? {
@@ -106,11 +106,9 @@ fn sm_event(ctx: &Context, args: Vec<String>) -> RedisResult {
         }
         None => {
             Err("machine doesn't exists".into())
-
         }
     }
 }
-
 
 redis_module! {
     name: "statemachine",
--- src/machine.rs
+++ src/machine.rs
@@ -1,25 +1,29 @@
 use std::borrow::Borrow;
 use std::collections::HashMap;
+use redis_module::raw;
+use std::os::raw::c_void;
+use std::os::raw::c_int;
+use std::convert::TryInto;
 
 pub type State = String;
 pub type Action = String;
 pub type Event = String;
 
 #[derive(Debug)]
-struct Transition {
-    state: State,
-    action: Action,
+pub struct Transition {
+    pub state: State,
+    pub action: Action,
 }
 
 #[derive(Debug)]
 pub struct SMachine {
-    state: State,
-    name: String,
-    transitions: HashMap<String, Transition>,
+    pub state: State,
+    pub name: String,
+    pub transitions: HashMap<String, Transition>,
 }
 
 impl SMachine {
-    pub fn new(state: State, name: String) -> SMachine{
+    pub fn new(state: State, name: String) -> SMachine {
         SMachine {
             state,
             name,
@@ -27,8 +31,20 @@ impl SMachine {
         }
     }
 
+    pub fn des(state: State, name: String, trans: HashMap<String, Transition>) -> SMachine {
+        SMachine {
+            state,
+            name,
+            transitions: trans,
+        }
+    }
+
     pub fn get_state(&mut self) -> String {
         self.state.clone()
+    }
+
+    pub fn get_name(&mut self) -> String {
+        self.name.clone()
     }
 
     pub fn add_transition(&mut self, start: State, event: Event, end: State, action: Action) {
@@ -39,9 +55,9 @@ impl SMachine {
     pub fn on_event(&mut self, event: Event) -> Action {
         let option = self.transitions.get(SMachine::key(self.state.clone(), event).as_str());
         match option {
-            Some(trans)=>{
+            Some(trans) => {
                 let trans = option.unwrap().clone();
-                self.state=trans.state.clone();
+                self.state = trans.state.clone();
                 trans.action.clone()
             }
             None => {
@@ -53,4 +69,44 @@ impl SMachine {
     fn key<'a>(start: State, event: Event) -> String {
         [start.as_str(), event.as_str()].concat()
     }
+}
+
+
+#[no_mangle]
+pub unsafe extern "C" fn RedisMachineRdbSave(rdb: *mut raw::RedisModuleIO, value: *mut c_void) {
+    if value.is_null() {
+        return;
+    }
+    let m = &*(value as *mut SMachine);
+    raw::save_string(rdb, &m.name.to_string());
+    raw::save_string(rdb, &m.state.to_string());
+    let mut trans = &m.transitions;
+    let i = trans.len() as u64;
+    raw::save_unsigned(rdb, i);
+    for (k, v) in trans {
+        raw::save_string(rdb, k);
+        raw::save_string(rdb, &v.state.clone());
+        raw::save_string(rdb, &v.action.clone());
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn RedisMachineRdbLoad(rdb: *mut raw::RedisModuleIO, encver: c_int) -> *mut c_void {
+    let name = raw::load_string(rdb);
+    let state = raw::load_string(rdb);
+    let num_trans = raw::load_unsigned(rdb);
+    let mut map = HashMap::new();
+    for _ in 0..num_trans {
+        let key = raw::load_string(rdb);
+        let state = raw::load_string(rdb);
+        let action = raw::load_string(rdb);
+        map.insert(key, Transition { state, action });
+    }
+    let mut omachine = SMachine::des(state, name, map);
+    Box::into_raw(Box::new(omachine)) as *mut c_void
+}
+
+
+pub unsafe extern "C" fn Free(value: *mut c_void) {
+    Box::from_raw(value as *mut SMachine);
 }
